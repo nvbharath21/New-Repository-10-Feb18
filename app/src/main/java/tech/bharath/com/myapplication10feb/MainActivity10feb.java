5813eb36c5612b715fe4e5ececfc240e55374989
nvbharath21name <bhrtandrdtst@gmail.com>
2018-02-10 17:01:31 +0530

    Training

---
+++ app/src/main/java/tech/bharath/com/myapplication10feb/MainActivity10feb.java
@@ -0,0 +1,13 @@
+package tech.bharath.com.myapplication10feb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity10feb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity10feb);
+    }
+}
